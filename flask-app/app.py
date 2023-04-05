1d1839564025d2bc5139a80aa3f60901c1a5485e
ConnerLusk <lusk.c@northeastern.com>
2023-04-05 10:08:51 -0400

    ahh

--- flask-app/app.py
+++
@@ -1,7 +0,0 @@
-from src import create_app
-
-# create the app object
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug = True, host = '0.0.0.0', port = 4000)
--- flask-app/src/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flaskext.mysql import MySQL
-
-db = MySQL()
-
-def create_app():
-    app = Flask(__name__)
-    return app
